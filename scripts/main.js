0fbf4f3f04067b9b1ec79d3e084132a115229a90
花杨永瀛 <3692993536@qq.com>
2026-02-15 20:16:31 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,13 @@
 importPackage(Packages.mindustry.graphics)
 
+const lib = Vars.mods.locateMod("神恒之心").meta;
+
 MapResizeDialog.minSize = 0
 MapResizeDialog.maxSize = 1000
 require("星球/神歆堕星");
 require('qwer');
 require("死焉");
+require("base/GHlib");
+require("icepeak/lib");
+require("icepeak/输出投位器");
+require("icepeak/输入投位器");
